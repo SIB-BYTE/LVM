2f0879d424f2bb5ded7fe6e9b3d79b155907aaca
SIB-BYTE <97762248+SIB-BYTE@users.noreply.github.com>
2022-06-19 19:15:44 +0000

    Add files via upload

---
+++ src/flags.c
@@ -0,0 +1,27 @@
+#include "../includes/vcpu.h"
+
+void clear_flags(cpu_t *cpu)
+{
+	cpu->lf ^= cpu->lf;
+    cpu->zf ^= cpu->zf;
+    cpu->gf ^= cpu->gf;
+}
+
+void set_flags(cpu_t *cpu, long src, long dest)
+{
+	long status = (src - dest);
+
+	cpu->zf = (status == 0);
+	cpu->lf = (status < 0);
+	cpu->gf = (status > 0);
+}
+
+void fset_flags(cpu_t *cpu, float src, float dest)
+{
+	float status = (src - dest);
+
+	cpu->zf = (status == 0);
+	cpu->lf = (status < 0);
+	cpu->gf = (status > 0);
+}
+
